69a79519d52d8c5031445833fbba8797ba2b5558
premkumar1428 <premjan1994@gmail.com>
2021-06-01 07:18:26 +0530

    01/06/2021 works

---
+++ src/org/scan/Students.java
@@ -0,0 +1,14 @@
+package org.scan;
+
+import java.util.Scanner;
+
+public class Students {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println(" ");
+	}
+
+}
---
+++ src/org/scan/workers.java
@@ -0,0 +1,61 @@
+package org.scan;
+
+import java.util.Scanner;
+
+public class workers {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("what is your empId");
+		
+		int empId = scan.nextInt();
+		
+		System.out.println("your emp id is ="+empId);
+		
+		System.out.println("what is your empName");
+		
+		String empName = scan.next();
+		
+		System.out.println("your emp name is ="+empName);
+		
+		System.out.println("what is your empEmail");
+		
+		String empEmail = scan.next();
+		
+		System.out.println("your emp email is ="+empEmail);
+		
+		System.out.println("what is your empPhoneno");
+		
+		String empPhoneno = scan.next();
+		
+		System.out.println("your phone no is ="+empPhoneno);
+		
+		System.out.println("what is your empSalary");
+		
+		float empSalary = scan.nextFloat();
+		
+		System.out.println("emp salary is ="+empSalary);
+		
+		System.out.println("what is your empGender");
+		
+		String empGender = scan.next();
+		
+		System.out.println("emp gender is ="+empGender);
+		
+		System.out.println("what is your emp city");
+		
+		String empCity = scan.next();
+		
+		System.out.println("emp city is ="+empCity);
+		
+	
+		
+		
+		
+		
+	
+	}
+
+}
